0a1c572a61360ed286939716b0a3d2401d4501c1
Daan Wijns <dw.daanwijns@gmail.com>
2019-11-08 14:41:53 +0100

    asp.net razer

---
+++ ASP.NET/WebApplication2 (Razer)/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2__Razer_.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication2__Razer_.Controllers
+{
+    public class HomeController : Controller
+    {
+        //TODO 02: Homecontroller maken
+
+        public ViewResult Index()
+        {
+            Product product = new Product
+            {
+                ProductID = 1,
+                Name = "Kayak",
+                Description = "A boat for one person.",
+                Category = "Watersports",
+                Price = 275M
+            };
+
+            //Gebruik het standaard 'overkoepelend' object
+            //en voeg zelf properties toe... Zoals StockLevel.
+            ViewBag.StockLevel = 2;
+
+            //product meegeven aan view
+            return View(product);
+        }
+    }
+}
---
+++ ASP.NET/WebApplication2 (Razer)/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2__Razer_.Models
+{
+    public class Product
+    {
+        //TODO 01: Klasse maken
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+}
